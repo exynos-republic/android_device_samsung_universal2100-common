3614bed7fab0e052dce0edfdb76b272fac62a191
TQMatvey <maybeetube@gmail.com>
2023-10-18 14:43:28 +0700

    universal2100-common: Import task_profiles from stock
    from G998BXXU9EWH5
    
    Change-Id: I90d476883af59b04c028f182bfc22961739d477e

--- common.mk
+++ common.mk
@@ -227,7 +227,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.samsung-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(COMMON_PATH)/configs/power/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Protobuf
 PRODUCT_PACKAGES += \

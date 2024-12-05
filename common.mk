e99de26c602909f8723eb32112e56d9a4acc1873
Mezaque <mezaquegit@gmail.com>
2024-12-05 13:38:02 -0300

    universal2100-common: Move NFC for configs/nfc
    Change-Id: Ia245d09e8337b2fea0a5bf5ab36f621af3dd902b

--- common.mk
+++ common.mk
@@ -303,8 +303,8 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Thermal
 PRODUCT_PACKAGES += \
